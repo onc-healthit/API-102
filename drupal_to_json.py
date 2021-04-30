9a441f43923bae9539f8380972fa4e7e0997a3dc
Keith Carlson <imkacarlson@gmail.com>
2021-04-30 12:05:08 -0400

    Caching files

--- drupal_to_json.py
+++ drupal_to_json.py
@@ -1,8 +1,30 @@
 import requests
+import json
 
 base_url = "https://healthit.gov/test-method"
 criterion = "standardized-api-patient-and-population-services"
 
+# entity_ids_json = requests.get("{}/{}?_format=json".format(base_url, criterion)).json()["field_clarification_table"]
+# Reading cached file (temporary)
+entity_ids_json = None
+with open('cached_entities.json') as f:
+  entity_ids_json = json.load(f)["field_clarification_table"]
 
-entity_ids = requests.get("{}/{}?_format=json".format(base_url, criterion))
-print(entity_ids.json())
+data_url = "https://healthit.gov/entity/paragraph"
+
+ccg_json = {criterion : []}
+
+paragraph_identifier = 1
+
+for entity_id in entity_ids_json:
+    #data_json = requests.get("{}/{}?_format=json".format(data_url, entity_id["target_id"])).json()
+    # Reading cached file (temporary)
+    data_json = None
+    with open('cached_response.json') as f:
+        data_json = json.load(f)
+
+    print(data_json["field_standard_s_referenced"])
+
+    break
+
+#print(entity_ids_json)
